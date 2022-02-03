cb112d1a0def6d482a5a658b29564a20a8c820c5
deimar <deimar.parn@khk.ee>
2022-02-03 12:57:36 +0000

    “Displayed all articles”

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const path = require('path')
 const hbs = require('express-handlebars');
 // setup template engine directory and files extensions
 app.set('views', path.join(__dirname, 'views'));
-app.set('views engine', 'hbs');
-app.engine('hbs', hbs.engine( {
+app.set('view engine', 'hbs');
+app.engine('hbs', hbs.engine({
     extname: 'hbs',
     defaultLayout: 'main',
     layoutsDir: __dirname + '/views/layouts',
@@ -29,6 +29,20 @@ con.connect(function (err)  {
     if(err) throw err;
     console.log("Connected to joga_mysql db")
 })
+
+// show
+app.get('/', (req, res) => {
+    let query = "SELECT * FROM article";
+    let articles = []
+    con.query(query, (err, result) => {
+        if (err) throw err;
+        articles = result
+        console.log(result)
+        res.render('index',{
+        articles: articles
+        })
+    })
+});
 
 app.listen(3000, () =>{
     console.log('App is started at http://localhost:3000');
