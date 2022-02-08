61949f3054f066c0f7ca62ea3d94cef63a492b4d
deimar <deimar.parn@khk.ee>
2022-02-08 13:32:17 +0000

    “Displayed author article list”

---
+++ controllers/article.js
@@ -0,0 +1,26 @@
+// show all articles - index page
+const getAllArtticles = (req, res) => {
+    let query = "SELECT * FROM article";
+    let articles = []
+    con.query(query, (err, result) => {
+        if (err) throw err;
+        articles = result
+        res.render('index', {
+            articles: articles
+        })
+    })
+};
+
+//show article by this slug
+const getArticleBySlug = (req, res) => {
+    let query = `SELECT *, article.name as article_name, auhtor.name as author_name FROM article INNER JOIN author ON author.id = article.author_id WHERE slug="${req.parms.slug}"`
+    let article
+    con.query(query, (err, result) => {
+        if (err) throw err;
+        article = result
+        console.log(article)
+        res.render('article', {
+            article: article
+        })
+    });
+};
